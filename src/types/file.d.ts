32f2f4307c0acc3b35d8968fd4e0e79175ad100f
Pham Tuan <tuan.pv@ptf.com.vn>
2023-05-22 16:34:28 +0700

    add packages

---
+++ src/types/file.d.ts
@@ -0,0 +1,26 @@
+declare module '*.mp4' {
+    export default string;
+}
+
+declare module '*.png' {
+    export default string;
+}
+
+declare module '*.xlsx' {
+    export default string;
+}
+
+
+declare module '*.xlsm' {
+    export default string;
+}
+
+declare module '*.csv' {
+    export default string;
+}
+
+declare module 'react-icons/*';
+
+declare module 'uuid';
+
+declare module 'lodash';
