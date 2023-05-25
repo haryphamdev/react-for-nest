bbde5b97778db6d4c7fbbe7ef023bcf3e52cdc02
Pham Tuan <tuan.pv@ptf.com.vn>
2023-05-25 09:41:30 +0700

    u

--- src/components/client/data-table/index.tsx
+++ src/components/client/data-table/index.tsx
@@ -5,6 +5,9 @@ import {
 } from '@ant-design/pro-components';
 //   import { getLocale } from '@umijs/max';
 // import styles from './styles.module.scss';
+import vi_VN from 'antd/locale/vi_VN';
+import enUS from 'antd/lib/locale/en_US';
+import { ConfigProvider } from 'antd';
 
 const DataTable = <
     T extends Record<string, any>,
@@ -17,10 +20,10 @@ const DataTable = <
     postData,
     // locale = getLocale(),
     pagination,
-    sticky = { offsetHeader: 50 },
+    // sticky = { offsetHeader: 50 },
     loading,
     rowKey = (record) => record.id,
-    scroll = { x: 2000 },
+    // scroll = { x: 2000 },
     params,
     request,
     search,
@@ -32,28 +35,30 @@ const DataTable = <
     rowSelection,
 }: ProTableProps<T, U, ValueType>) => {
     return (
-        <ProTable<T, U, ValueType>
-            // className={styles.table}
-            columns={columns}
-            defaultData={defaultData}
-            dataSource={dataSource}
-            postData={postData}
-            // locale={locale}
-            pagination={pagination}
-            sticky={sticky}
-            loading={loading}
-            rowKey={rowKey}
-            scroll={scroll}
-            params={params}
-            request={request}
-            search={search}
-            polling={polling}
-            toolBarRender={toolBarRender}
-            headerTitle={headerTitle}
-            actionRef={actionRef}
-            dateFormatter={dateFormatter}
-            rowSelection={rowSelection}
-        />
+        <ConfigProvider locale={vi_VN}>
+            <ProTable<T, U, ValueType>
+                // className={styles.table}
+                columns={columns}
+                defaultData={defaultData}
+                dataSource={dataSource}
+                postData={postData}
+                pagination={pagination}
+                bordered
+                // sticky={sticky}
+                loading={loading}
+                rowKey={rowKey}
+                // scroll={scroll}
+                params={params}
+                request={request}
+                search={search}
+                polling={polling}
+                toolBarRender={toolBarRender}
+                headerTitle={headerTitle}
+                actionRef={actionRef}
+                dateFormatter={dateFormatter}
+                rowSelection={rowSelection}
+            />
+        </ConfigProvider>
     );
 };
 
