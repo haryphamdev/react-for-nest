02bdbb6a3016845fd0d575ce19a03080e8e7cf1f
Quynh Tran <haryphamdev@gmail.com>
2023-10-14 16:30:35 +0700

    update run build

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 ### Source code frontend khóa học Nest.JS
 Tác giả: Hỏi Dân IT
 
-Các bước cài đặt:
+Các bước cài đặt: (chế độ development)
 1. clone code
 2. cài đặt thư viện: npm i
 3. Update file .env (nếu cần thiết)
 4. Chạy dự án: npm run dev
+
+===
+Cách chạy tại chế độ production:
+1. clone code
+2. cài đặt thư viện: npm i
+3. Update file .env (nếu cần thiết)
+4. Build dự án: npm run build
+5. Chạy dự án: npm run preview
--- src/components/admin/user/view.user.tsx
+++ src/components/admin/user/view.user.tsx
@@ -29,7 +29,7 @@ const ViewDetailUser = (props: IProps) => {
                     <Descriptions.Item label="Tuổi">{dataInit?.age}</Descriptions.Item>
 
                     <Descriptions.Item label="Vai trò" >
-                        <Badge status="processing" text={dataInit?.role} />
+                        <Badge status="processing" text={<>{dataInit?.role}</>} />
                     </Descriptions.Item>
                     <Descriptions.Item label="Địa chỉ" >{dataInit?.address}</Descriptions.Item>
                     <Descriptions.Item label="Thông tin công ty" span={2}>
